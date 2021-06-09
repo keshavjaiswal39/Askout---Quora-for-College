3adc7a234074c6dac25302fa1bf875a32e121d86
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-06-09 23:39:56 +0530

    Added dependencies like @material-ui/core @material-ui/icons firebase react-flip-move react-modal

---
+++ src/component/Askout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Askout.css';
+
+function Askout() {
+    return (
+        <div className="askout">
+            Hello from the quora container component
+        </div>
+    )
+}
+
+export default Askout;
