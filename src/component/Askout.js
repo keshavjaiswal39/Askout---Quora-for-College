44ed7b1a3d856ad24b3b65d26359fcb7655b672d
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-06-09 23:43:06 +0530

    Added components

--- src/component/Askout.js
+++ src/component/Askout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../css/Askout.css';
+import Navbar from './Navbar';
 
 function Askout() {
     return (
         <div className="askout">
             Hello from the quora container component
+            <Navbar />
         </div>
     )
 }
---
+++ src/component/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            Hello from Navigation bar component
+        </div>
+    )
+}
+
+export default Navbar;
