9cb4a3e0ad09f11f1ca89e708526919c0a513a9e
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-06-10 23:00:43 +0530

    Added changes in  styling of the Navbar component

--- src/component/Askout.js
+++ src/component/Askout.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../css/Askout.css';
 import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 
 function Askout() {
     return (
         <div className="askout">
 
             <Navbar />
+            <div className="askout_content">
+                <Sidebar />
+            </div>
         </div>
     )
 }
